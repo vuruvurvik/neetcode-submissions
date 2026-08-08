ad5efe7c8bc7c07b6d59327d0d7e362e5d254540
vurvik.korukonda <145532760+vuruvurvik@users.noreply.github.com>
2026-08-08 22:17:22 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(nums.begin(),nums.end());
+        ans.insert(ans.end(),nums.begin(),nums.end());
+        return ans;
+    }
+};

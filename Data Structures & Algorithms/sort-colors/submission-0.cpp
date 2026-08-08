bec002150455619fcd60ba3223db0bac2207f7f2
vurvik.korukonda <145532760+vuruvurvik@users.noreply.github.com>
2026-08-08 23:28:02 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0,mid=0,r=nums.size()-1;
+        while(mid<=r)
+        {
+            if(nums[mid]==0)swap(nums[l++],nums[mid++]);
+            else if(nums[mid]==1)mid++;
+            else
+            swap(nums[mid],nums[r--]);
+        }
+    }
+};

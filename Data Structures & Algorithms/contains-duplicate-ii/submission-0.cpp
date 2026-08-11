0dfe6f279589497ec3b2f7cb7706d8a960f789ea
vurvik.korukonda <145532760+vuruvurvik@users.noreply.github.com>
2026-08-11 17:39:10 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int>mpp;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mpp.find(nums[i])!=mpp.end())
+            {
+                if(abs(i-mpp[nums[i]])<=k)return true;
+            }
+            mpp[nums[i]]=i;
+        }
+        return false;
+    }
+};

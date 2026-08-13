d8c83aac2a57c2a78443f27119f321fac0cbb34a
vurvik.korukonda <145532760+vuruvurvik@users.noreply.github.com>
2026-08-13 19:27:46 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return find(nums.begin(),nums.end(),target)!=nums.end()?
+        find(nums.begin(),nums.end(),target)-nums.begin():-1;
+    }
+};
